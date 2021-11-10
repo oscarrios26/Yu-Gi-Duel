28fdbd08d26897b735d7d576578a518690d6b5aa
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2021-11-10 11:10:06 -0600

    project1

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 
     
     
-    
+    function displayCards(card){
+console.log(card);
+    }
     
     
     
     
     
     async function fetchCards() {
-
+    try {
     let cards = await axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php");
     
     console.log(cards);
-
+    displayCards(cards.data);
+    }catch (error) {
+    console.log(error);
+    }finally {
+        console.log("DONE");
     }
-
+}
     fetchCards();
