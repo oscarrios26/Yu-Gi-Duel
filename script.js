75dc2fe9c49371e773f546c83a8466662b043e4c
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2021-11-11 16:51:19 -0600

    created a random array

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 
     
-    let thumbnail = document.querySelector(".leftaside");
-    let leftPlayer = document.querySelector(".resetbtn");
+    let leftplayer = document.querySelector(".leftaside");
+    // let = document.querySelector(".resetbtn");
+
+     // let thumbnail =document.createElement("img");
+            // thumbnail.setAttribute("src", allCards[i]);
+            // leftplayer.appendChild(thumbnail)
         
+    newArr =[];
+
     function displayCards(images) {
         
         for (let i = 0; i < 50; i++){
-            // console.log(images[i].card_images[0].image_url)
+            //  console.log(images[i].card_images[0].image_url)
             let imagesOfCards = images[i].card_images[0].image_url;
-            // console.log(imagesOfCards);
-            allCards=[imagesOfCards];
-            console.log(allCards);
+        
+            newArr.push(imagesOfCards)
         }
+
+           
+
+            // console.log(newArr);
+    const randomItem = newArr[Math.floor(Math.random()*newArr.length)];
+    console.log(randomItem);
     };
 
     
-     
+
+
 
     
     async function fetchCards() {
     try {
     let cards = await axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php");
     let images = cards.data.data;
-    console.log(images);
-    console.log(typeof images)
+    // console.log(images);
+    // console.log(typeof images)
     displayCards(images);
     }catch (error) {
     console.log(error);
